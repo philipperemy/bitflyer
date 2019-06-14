2d7856ede39b9ca3bf0459c21be5ee344b35705d
philipperemy <premy@cogent.co.jp>
2019-06-14 15:10:25 +0900

    add REST API

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup
 
 setup(
     name='bitflyer-rt',
-    version='1.0',
-    description='Bitflyer Realtime Feed',
+    version='1.1',
+    description='Bitflyer Realtime and Rest API',
     author='Philippe Remy',
     license='MIT',
     long_description_content_type='text/markdown',
     long_description=open('README.md').read(),
     packages=['bitflyer'],
     install_requires=[
-        'websocket-client==0.47.0'
+        'websocket-client==0.47.0',
+        'pybitflyer==0.1.9'
     ]
 )
