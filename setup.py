4834fdf23201be9fd64379bfa5d84c92dd5ca9bb
Philippe Remy <premy.enseirb@gmail.com>
2020-06-15 14:42:41 +0900

    2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bitflyer-rt',
-    version='2.1',
+    version='2.2',
     description='Bitflyer Realtime and Rest API',
     author='Philippe Remy',
     license='MIT',
